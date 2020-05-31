90bef7e05cc393852de3f25638f7ed9831a27b83
JouleV <joulev.vvd@yahoo.com>
2020-05-31 12:11:30 +0700

    Set up continuous integration (1)

---
+++ ci/texlive_install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+
+# Modified from https://github.com/latex3/latex3/blob/master/support/texlive.sh
+
+# This script is used for testing using Travis
+# It is intended to work on their VM set up: Ubuntu 12.04 LTS
+# A minimal current TL is installed adding only the packages that are
+# required
+
+# See if there is a cached version of TL available
+export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
+if ! command -v texlua > /dev/null; then
+  # Obtain TeX Live
+  wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+  tar -xzf install-tl-unx.tar.gz
+  cd install-tl-20*
+
+  # Install a minimal system
+  ./install-tl --profile=../texlive.profile
+
+  cd ..
+fi
+
+# Update tlmgr itself
+tlmgr update --self
+
+# The test framework itself
+tlmgr install l3build
+
+# Build the documentation using XeLaTeX
+tlmgr install xetex
+
+# Then get the rest of required LaTeX
+tlmgr install amsmath graphics tools
+
+# Assuming a 'basic' font set up, metafont is required to avoid
+# warnings with some packages and errors with others
+tlmgr install metafont mfware texlive-scripts
+
+# Contrib packages for testing
+# nothing for now
+
+# Keep no backups (not required, simply makes cache bigger)
+tlmgr option -- autobackup 0
+
+# Update the TL install but add nothing new
+tlmgr update --self --all --no-auto-install
