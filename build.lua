0dd30c0072c0d68c3aeb00d138f06cca69a22088
JouleV <joulev.vvd@yahoo.com>
2020-06-08 13:34:16 +0700

    Add test files for all code examples

--- build.lua
+++ build.lua
@@ -2,6 +2,8 @@
 
 module = "akshar"
 typesetexe = "xelatex"
+checkengines = {"xetex"}
+stdengine = "xetex"
 excludefiles = {"README.md", "TODO.md"}
 textfiles = {"README.txt"}
 sourcefiles = {"fonts/*.ttf", "fonts/*.otf", "*.dtx", "*.ins"}
